7f76691b9a90a06e03d5552980a72394e598e093
flaviaramirez <flaviagiselaramirez@gmail.com>
2022-07-30 22:57:00 -0300

    ruteado

--- src/main/java/com/portfolio/fgr/Controller/CCursos.java
+++ src/main/java/com/portfolio/fgr/Controller/CCursos.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cursos")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://flaviaramirez.firebaseapp.com")
 public class CCursos {
 
     @Autowired
--- src/main/java/com/portfolio/fgr/Controller/CEducacion.java
+++ src/main/java/com/portfolio/fgr/Controller/CEducacion.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://flaviaramirez.firebaseapp.com")
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
